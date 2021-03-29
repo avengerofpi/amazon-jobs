72bf6b247b791f033c9986879c0f36afbbb74df8
Cory Washington <tbd>
2021-03-28 22:29:40 -0400

    add call to get current jobs ads from Amazon API
    Save to a new file with embedded timestamp (precise to the second).

--- amazon-jobs-parser.sh
+++ amazon-jobs-parser.sh
@@ -4,6 +4,37 @@
 # See `man bash` or `man bash-builtins` for more details (search for 'set')
 set -eu;
 
+# Get current desired job ads. This should also set the global 'jobsFile' property.
+function getJobs() {
+  # Declare base URL resource.
+  local urlResource="https://jobs-us-east.amazon.com/en/search.json";
+  # Setup the URL parameters.
+  local urlParameters="?";
+  urlParameters+="category[]=software-development&";
+  urlParameters+="schedule_type_id[]=Full-Time&";
+  urlParameters+="normalized_location[]=Arlington%2C%20Virginia%2C%20USA&";
+  urlParameters+="normalized_location[]=Herndon%2C%20Virginia%2C%20USA&";
+  urlParameters+="normalized_location[]=Baltimore%2C%20Maryland%2C%20USA&";
+  #urlParameters+="normalized_location[]=USA&";
+  urlParameters+="business_category[]=amazon-web-services&";
+  urlParameters+="radius=24km&";
+  urlParameters+="facets[]=location&";
+  urlParameters+="facets[]=business_category&";
+  urlParameters+="facets[]=category&";
+  urlParameters+="facets[]=schedule_type_id&";
+  urlParameters+="facets[]=employee_class&";
+  urlParameters+="facets[]=normalized_location&";
+  urlParameters+="facets[]=job_function_id&";
+  urlParameters+="offset=0&";
+  urlParameters+="result_limit=10000&";
+  # Compose the final URL.
+  local url="${urlResource}${urlParameters}";
+  # Declare the filename to send job ads JSON to. Try to embed a timestamp.
+  # This should be a non-local/global property.
+  jobsFile="jobs.aws.md-local.software-development.$(date +%F--%Hh%Mm%Ss).json";
+  curl -sS "${url}" | jq -M . > ${jobsFile};
+}
+
 # Try to create a fresh job details directory to use
 jobDetailsDir="";
 jobDetailsDirDefault="jobDetails";
@@ -25,9 +56,12 @@ else
   exit 1;
 fi;
 
-# Get number of job advertizements. Exit if none were found.
+# Get current job ads. This should also set the global 'jobsFile' property.
+getJobs;
 #jobsFile="jobs.aws.json";
-jobsFile="jobs.aws.md-local.software-development.2021-03-02.json";
+#jobsFile="jobs.aws.md-local.software-development.2021-03-02.json";
+
+# Get number of job advertizements. Exit if none were found.
 [ -r "${jobsFile}" ] || { echo "Error: the jobs file '${jobsFile}' does not exist or cannot be read" && exit 3; }
 echo "Processing jobs file '${jobsFile}'";
 totalNumJobs="`jq '.jobs | length' ${jobsFile}`";
